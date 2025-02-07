3171337ddf5b82c74f6678af74192d3be97cac4a
Dhruv Taraviya <dhruvtaraviya1@gmail.com>
2025-02-07 18:56:26 +0530

    Added Navbar and Main section

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import Nav from './Components/Nav';
+import Main from './Components/Main';
 
 function App() {
   return (
     <>
+    <Nav title="Profile"></Nav>
+
+    <div className="container">
+      <Main style={{maxwidth: "540px"}}></Main>
+    </div>
     </>
   );
 }
