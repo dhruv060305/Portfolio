94b98985fc37df4a2afe6df3d7cf6faf863a3fb5
Dhruv Taraviya <dhruvtaraviya1@gmail.com>
2025-02-09 17:07:33 +0530

    added 1st section

--- src/App.js
+++ src/App.js
@@ -8,9 +8,10 @@ function App() {
     <Nav title="Profile"></Nav>
 
     <div className="container">
-      <Main style={{maxwidth: "540px"}}></Main>
+      <Main style={{maxwidth: "540px"}} cardTitle="Dhruv Taraviya" about="LinkedIn: " cardText="I'm Dhurv Taraviya, a passionate front-end developer with expertise in HTML, CSS, JavaScript, and React. I specialize in building responsive and user-friendly web applications. My goal is to create seamless digital experiences with clean and efficient code. I constantly explore new technologies to enhance my skills and stay updated with industry trends"></Main>
     </div>
     </>
+
   );
 }
 
--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,19 +1,30 @@
 import React from 'react'
+import PP from './Snapchat-1351010918.jpg';
 
 export default function Main(props) {
     return (
         <div>
 
-            <div class="card mb-3" style ={props.style}>
-                <div class="row g-0">
-                    <div class="col-md-4">
-                        <img src="/" class="img-fluid rounded-start" alt="..."/>
+            <div className="card mb-3 my-3" style={props.style}>
+                <div className="row g-0">
+                    <div className="col-md-4">
+                        <img src={PP} className="img-fluid rounded-start my-4" alt="..." />
                     </div>
-                    <div class="col-md-8">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                    <div className="col-md-8">
+                        <div className="card-body">
+                            <h5 className="card-title">{props.cardTitle}</h5>
+                            <p className="card-text">
+                                
+                                {props.cardText}
+
+                                {props.about} 
+                                
+                                
+                            </p>
+                            <p> <a href="https://www.linkedin.com/in/dhruv-taraviya-103010349/" target="_blank" rel="noopener noreferrer">
+                                Visit My LinkedIn
+                            </a></p>
+                            <p className="card-text"><small className="text-body-secondary"></small></p>
                         </div>
                     </div>
                 </div>
